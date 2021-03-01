13b8c7c4bd3955a315f56cfa2241f7e939247fee
acortes31 <a.cortes31@hotmail.com>
2021-03-01 11:09:49 -0500

    portafolio

--- js/js.js
+++ js/js.js
@@ -1,10 +1,17 @@
 
 function cambiar_tamaño_height() {
+    var width_ventana= $(window).width();
     var height_sidebar = $(".sidebar").height();
     var height_ventana = $(window).height();
     console.log(height_sidebar);
     console.log(height_ventana);
-    if (height_sidebar < height_ventana) {
-        $(".sidebar").height('100vh');
+    if(width_ventana>768) {
+        if (height_sidebar < height_ventana) {
+            $(".sidebar").height('100vh');
+        }
     }
+    $("#header").click(function (){
+
+        $(".sidebar").css('visibility','visible');
+    });
 }
